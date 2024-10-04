6c25c959ea4bd3a9a9369b41efd34aae39764d84
123456789afruz <afruzoxunov@gmail.com>
2024-10-04 19:01:54 +0500

    Add files via upload

--- js.js
+++ js.js
@@ -7,11 +7,29 @@ burger.addEventListener('click', () => {
 
 const swiperAlbum1 = new Swiper('.swiper-album-1', {
     loop: true,
-    slidesPerView: 1,
+    slidesPerView: 0.3,
+    spaceBetween: 8,
+    
     autoplay: {
         delay: 0,
         disableOnInteraction: false,
     },
+
+    breakpoints: {
+        500: {
+          slidesPerView: 0.4,
+        //   spaceBetween: 50,
+        },
+        700: {
+          slidesPerView: 0.5,
+          
+        },
+        900: {
+          slidesPerView: 0.6,
+          
+        },
+      },
+
     allowTouchMove: false,
 
     speed: 20000
@@ -20,12 +38,28 @@ const swiperAlbum1 = new Swiper('.swiper-album-1', {
 
 const swiperAlbum2 = new Swiper('.swiper-album-2', {
     loop: true,
-    slidesPerView: 1,
+    slidesPerView: 0.3,
+    spaceBetween: 8,
+
     autoplay: {
         delay: 0,
         disableOnInteraction: false,
         reverseDirection: true
     },
+    breakpoints: {
+        500: {
+          slidesPerView: 0.4,
+        //   spaceBetween: 50,
+        },
+        700: {
+          slidesPerView: 0.5,
+          
+        },
+        900: {
+          slidesPerView: 0.6,
+          
+        },
+      },
     allowTouchMove: false,
 
     speed: 20000
